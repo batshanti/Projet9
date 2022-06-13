1a6bdd0771eb68c45f66b7b6e2b253f96d900f53
batshanti <teddylouvet@yahoo.fr>
2022-06-13 16:11:25 +0200

    PEP8 modifications

--- LITReview/Review_Ticket/utils.py
+++ LITReview/Review_Ticket/utils.py
@@ -1,33 +1,16 @@
 from Review_Ticket.models import UserFollows, Ticket, Review
 from django.contrib.auth.models import User
 
-# class Database:
-
-#     def __init__(self, name=""):
-#         self.name = name
-#         self.user = User.objects.get(username=self.name)
-
-#     def get_user_follow(self, name):
-#         pass
-
-#     def create_review_ticket(self, form_ticket, form_review):
-#         ticket = form_ticket.save(commit=False)
-#         ticket.user = self.user
-#         ticket.save()
-#         review = form_review.save(commit=False)
-#         review.ticket = ticket
-#         review.headline = ticket.title
-#         review.user = self.user
-#         review.save()
-
 
 def get_ticket(pk):
     ticket = Ticket.objects.get(pk=pk)
     return ticket
 
+
 def get_user_follow(user_log):
     user_bdd = User.objects.get(username=user_log)
     return UserFollows.objects.filter(user=user_bdd)
+
 
 def create_review_ticket(name, form_ticket, form_review):
     ticket = form_ticket.save(commit=False)
@@ -37,17 +20,16 @@ def create_review_ticket(name, form_ticket, form_review):
     review.ticket = ticket
     review.headline = ticket.title
     review.user = User.objects.get(username=name)
-    
+
 
 def get_ticket_user_follow(user_follow, user_log):
     users_follow = []
     for user in user_follow:
         users_follow.append(user.followed_user)
 
-
     tickets_follow = []
 
-    for user in users_follow :
+    for user in users_follow:
         tickets = Ticket.objects.filter(user=user)
         for ticket in tickets:
             tickets_follow.append(ticket)
@@ -59,9 +41,9 @@ def get_ticket_user_follow(user_follow, user_log):
 
     return tickets_follow
 
+
 def create_review_from_ticket(user_log, form_review, ticket):
     review = form_review.save(commit=False)
     review.ticket = ticket
-    review.user = User.objects.get(username=user_log) 
+    review.user = User.objects.get(username=user_log)
     review.save()
-
